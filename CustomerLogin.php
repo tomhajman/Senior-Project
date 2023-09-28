0b2c45fbb393266a077aa6b14d48134d091ad720
mrahmanr <58044335+mrahmanr@users.noreply.github.com>
2023-09-28 14:33:33 -0400

    Update CustomerLogin.php
    Spent 2 + 2 hours together for both login and customers home page with Bilal Siddiqui

--- CustomerLogin.php
+++ CustomerLogin.php
@@ -4,213 +4,92 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
 
-/* Full-width input fields */
 input[type=text], input[type=password] {
-width: 100%;
-padding: 12px 20px;
-margin: 8px 0;
-display: inline-block;
-border: 1px solid #ccc;
-box-sizing: border-box;
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
 }
 
-/* Set a style for all buttons */
 button {
-background-color: #04AA6D;
-color: white;
-padding: 14px 20px;
-margin: 8px 0;
-border: none;
-cursor: pointer;
-width: 100%;
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
 }
 
 button:hover {
-opacity: 0.8;
+  opacity: 0.8;
 }
 
-/* Extra styles for the cancel button */
 .cancelbtn {
-width: auto;
-padding: 10px 18px;
-background-color: #f44336;
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
 }
 
-/* Center the image and position the close button */
 .imgcontainer {
-text-align: center;
-margin: 24px 0 12px 0;
-position: relative;
+  text-align: center;
+  margin: 24px 0 12px 0;
 }
 
 img.avatar {
-width: 40%;
-border-radius: 50%;
+  width: 40%;
+  border-radius: 50%;
 }
 
 .container {
-padding: 16px;
+  padding: 16px;
 }
 
 span.psw {
-float: right;
-padding-top: 16px;
-}
-
-/ The Modal (background) /
-.modal {
-display: none; / Hidden by default /
-position: fixed; / Stay in place /
-z-index: 1; / Sit on top /
-left: 0;
-top: 0;
-width: 100%; / Full width /
-height: 100%; / Full height /
-overflow: auto; / Enable scroll if needed /
-background-color: rgb(0,0,0); / Fallback color /
-background-color: rgba(0,0,0,0.4); / Black w/ opacity /
-padding-top: 60px;
-}
-
-/ Modal Content/Box /
-.modal-content {
-background-color: #fefefe;
-margin: 5% auto 15% auto; / 5% from the top, 15% from the bottom and centered /
-border: 1px solid #888;
-width: 80%; / Could be more or less, depending on screen size /
-}
-
-/* The Close Button (x) */
-.close {
-position: absolute;
-right: 25px;
-top: 0;
-color: #000;
-font-size: 35px;
-font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-color: red;
-cursor: pointer;
-}
-
-/* Add Zoom Animation */
-.animate {
--webkit-animation: animatezoom 0.6s;
-animation: animatezoom 0.6s
-}
-
-@-webkit-keyframes animatezoom {
-from {-webkit-transform: scale(0)}
-to {-webkit-transform: scale(1)}
-}
-
-@keyframes animatezoom {
-from {transform: scale(0)}
-to {transform: scale(1)}
+  float: right;
+  padding-top: 16px;
 }
 
 /* Change styles for span and cancel button on extra small screens */
 @media screen and (max-width: 300px) {
-span.psw {
-display: block;
-float: none;
-}
-.cancelbtn {
-width: 100%;
-}
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
 }
 </style>
 </head>
 <body>
 
-<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+<h2>Login Form</h2>
 
-<div id="id01" class="modal">
-<form class="modal-content animate" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-<div class="imgcontainer">
-<span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">×</span>
-</div>
+<form action="/action_page.php" method="post">
 
-    <div class="container">
-        <label for="username"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="username" required>
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
 
-        <label for="password"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
 
-        <button type="submit">Login</button>
-         <a href="main.html">Back to Main Menu</a>
-    </div>
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
 </form>
-</div>
-
-<script>
-
-var modal = document.getElementById('id01');
-
-window.onclick = function(event) {
-if (event.target == modal) {
-modal.style.display = "none";
-}
-}
-document.querySelector("form").onsubmit = function() {
-function validateForm() {
-var username = document.forms[0]["username"].value;
-var password = document.forms[0]["password"].value;
-if (username.trim() === "" || password.trim() === "") {
-alert("Please enter both username and password.");
-return false;
-};
-</script>
-
-<?php
-//Mohammed Rahman
-//I certify that this submission is my own original work
-$servername = "localhost";
-$username = "usersu23";
-$password = "passwdsu23";
-$dbname = "bcs350su23";
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-$input_username = $_POST['username'];
-$input_password = $_POST['password'];
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
-$stmt->bind_param("s", $input_username);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($result->num_rows === 1) {
-
-    $row = $result->fetch_assoc();
-    $hashed_password = $row['password'];
-
-    if (password_verify($input_password, $hashed_password)) {
-
-        header("Location: index.html");
-        exit;
-    } else {
-
-        echo "Incorrect username or password.";
-    }
-} else {
-    echo "Incorrect username or password.";
-}
-$stmt->close();
-$conn->close();
-}
-?>
 
 </body>
 </html>
