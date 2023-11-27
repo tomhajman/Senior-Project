b8957eb3ddebadd0b7346aac8a02b580895da541
mrahmanr <58044335+mrahmanr@users.noreply.github.com>
2023-11-27 13:53:05 -0500

    Organized Css, Functional Ratings

--- contractorRatings.php
+++ contractorRatings.php
@@ -1,166 +1,147 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+include 'DBCredentials.php';
+$userEmail = $_SESSION['customerEmail'];
 
-<head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        header {
-            background-color: #333;
-            color: #fff;
-            padding: 24px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
+function connectToDB()
+{
+    global $HOST_NAME, $USERNAME, $PASSWORD, $DB_NAME, $conn;
+    $conn = new mysqli($HOST_NAME, $USERNAME, $PASSWORD, $DB_NAME);
 
-        .dropdown {
-            position: absolute;
-            left: 0;
-        }
-
-        .dropbtn {
-            background-color: #333;
-            color: #fff;
-            padding: 16px;
-            font-size: 24px;
-            border: none;
-        }
-
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #333;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            left: 0;
-            top: 100%;
-            z-index: 1;
-            text-align: left;
-        }
-
-        .dropdown-content a {
-            color: gray;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-        }
-
-        .dropdown-content a:hover {
-            background-color: #ddd;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-
-        .dropdown:hover .dropbtn {
-            background-color: #3e8e41;
-        }
-
-        .welcome-user {
-            margin-right: 10px;
-            margin-left: auto;
-        }
-
-        .checked {
-            color: orange;
-        }
-
-        .stars {
-            font-size: 24px;
-            cursor: pointer;
-        }
-
-        .centered-content {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f2f2f2;
-        }
-
-        .card {
-            background-color: #ffffff;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-        }
-
-        #thankYouMessage {
-            color: #3e8e41;
-            margin-top: 10px;
-        }
-    </style>
-</head>
-
-<body>
-    <?php
-    session_start();
-    include 'DBCredentials.php';
-    $userEmail = $_SESSION['customerEmail'];
-    $customerID = $_SESSION['customerID'];
-
-    function connectToDB()
-    {
-        global $HOST_NAME, $USERNAME, $PASSWORD, $DB_NAME, $conn;
-        $conn = new mysqli($HOST_NAME, $USERNAME, $PASSWORD, $DB_NAME);
-
-        if ($conn->connect_error) {
-            die("Connection issue: " . $conn->connect_error);
-        }
-        return $conn;
+    if ($conn->connect_error) {
+        die("Connection issue: " . $conn->connect_error);
     }
-    $db = connectToDB();
+    return $conn;
+}
 
-    function saveRating($qualityRating, $communicationRating, $timelinessRating, $priceRating, $contractorID, $jobID)
-    {
-        $db = connectToDB();
-        // Insert the ratings into the database
-        $insertRatingQuery = "INSERT INTO rating (qualityRating, communicationRating, timelinessRating, priceRating, contractorID, jobID)
-                            VALUES ('$qualityRating', '$communicationRating', '$timelinessRating', '$priceRating', '$contractorID', $jobID)";
-        $db->query($insertRatingQuery);
+$db = connectToDB();
 
-        // Calculate the average rating
-        $getRatings = $db->query("SELECT AVG(qualityRating) AS averageQuality, AVG(communicationRating) AS averageCommunication, AVG(timelinessRating) AS averageTimeliness, AVG(priceRating) AS averagePrice FROM rating WHERE contractorID = $contractorID");
-        if($getRatings){
-            $row = $getRatings->fetch_assoc();
-            $averageRating = ($row['averageQuality'] + $row['averageCommunication'] + $row['averageTimeliness'] + $row['averagePrice']) / 4;
-            // Update the contractor's overall rating
-            $updateContractorRatingQuery = "UPDATE contractor SET ContractorRating = '$averageRating' WHERE ContractorID = '$contractorID'";
-            $db->query($updateContractorRatingQuery);
-        } else {
-            die("Database error: " . $db->connect_error);
-        }
+$getFNameQuery = "SELECT customerFirstName FROM customer WHERE customerEmail = ?";
+$stmt = $db->prepare($getFNameQuery);
+$stmt->bind_param("s", $userEmail);
+$stmt->execute();
+$result = $stmt->get_result();
 
-        
+if ($result) {
+    $row = $result->fetch_assoc();
+    $userFName = $row['customerFirstName'];
+} else {
+    $userFName = "User";
+}
+
+$jobID = isset($_GET['jobID']) ? $_GET['jobID'] : null;
+
+$checkRatingQuery = "SELECT * FROM rating WHERE jobID = ? AND customerID = ?";
+// Moved outside of the if statement
+$stmtCheckRating = $db->prepare($checkRatingQuery);
+$stmtCheckRating->bind_param("ii", $jobID, $_SESSION['customerID']);
+$stmtCheckRating->execute();
+$resultCheckRating = $stmtCheckRating->get_result();
+
+if ($resultCheckRating) {
+    $rowCheckRating = $resultCheckRating->fetch_assoc();
+
+    if (!empty($rowCheckRating)) {
+        $prefilledQualityRating = $rowCheckRating['qualityRating'];
+        $prefilledCommunicationRating = $rowCheckRating['communicationRating'];
+        $prefilledTimelinessRating = $rowCheckRating['timelinessRating'];
+        $prefilledPriceRating = $rowCheckRating['priceRating'];
+    } else {
+        $prefilledQualityRating = 0;
+        $prefilledCommunicationRating = 0;
+        $prefilledTimelinessRating = 0;
+        $prefilledPriceRating = 0;
+    }
+} else {
+    $prefilledQualityRating = 0;
+    $prefilledCommunicationRating = 0;
+    $prefilledTimelinessRating = 0;
+    $prefilledPriceRating = 0;
+}
+
+function saveRating($qualityRating, $communicationRating, $timelinessRating, $priceRating, $contractorID, $jobID, $customerID)
+{
+    global $db, $checkRatingQuery;
+    $insertRatingQuery = "INSERT INTO rating (qualityRating, communicationRating, timelinessRating, priceRating, contractorID, jobID, customerID)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $db->prepare($insertRatingQuery);
+
+    if ($stmt === false) {
+        die('Error preparing statement: ' . $db->error);
     }
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if(isset($_GET['jobID']) && is_numeric($_GET['jobID'])){
-            $jobID = $_GET['jobID'];
-            $qualityRating = $_POST['qualityRating'];
-            $communicationRating = $_POST['communicationRating'];
-            $timelinessRating = $_POST['timelinessRating'];
-            $priceRating = $_POST['priceRating'];
+    $stmt->bind_param("iiiiiii", $qualityRating, $communicationRating, $timelinessRating, $priceRating, $contractorID, $jobID, $customerID);
 
-            // Fetch the job and contractor information from the database
-            $getJobInfoQuery = "SELECT ContractorID
-                                FROM customerJob
-                                WHERE jobID = $jobID";
-            $jobInfoResult = $db->query($getJobInfoQuery);
+    if ($stmt->execute()) {
+        // Fetch the updated ratings
+        $stmt->close();
+        $stmtCheckRating = $db->prepare($checkRatingQuery);
+        $stmtCheckRating->bind_param("ii", $jobID, $_SESSION['customerID']);
+        $stmtCheckRating->execute();
+        $resultCheckRating = $stmtCheckRating->get_result();
 
-            if ($jobInfoResult) {
-                $jobInfoRow = $jobInfoResult->fetch_assoc();
-                $contractorID = $jobInfoRow['ContractorID'];
+        if ($resultCheckRating) {
+            $row = $resultCheckRating->fetch_assoc();
 
-                // Save the ratings to the database
-                saveRating($qualityRating, $communicationRating, $timelinessRating, $priceRating, $contractorID, $jobID);
+            if (!empty($row)) {
+                return $row;
             }
         }
     }
 
-    $db->close();
-    ?>
+    return false;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['jobID']) && is_numeric($_POST['jobID'])) {
+        $jobID = $_POST['jobID'];
+        $qualityRating = $_POST['qualityRating'];
+        $communicationRating = $_POST['communicationRating'];
+        $timelinessRating = $_POST['timelinessRating'];
+        $priceRating = $_POST['priceRating'];
+
+        $getJobInfoQuery = "SELECT ContractorID, customerID FROM customerJob WHERE jobID = ?";
+        $stmt = $db->prepare($getJobInfoQuery);
+        $stmt->bind_param("i", $jobID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result) {
+            $jobInfoRow = $result->fetch_assoc();
+
+            if (!empty($jobInfoRow)) {
+                $contractorID = $jobInfoRow['ContractorID'];
+                $customerID = $jobInfoRow['customerID'];
+
+                $row = saveRating($qualityRating, $communicationRating, $timelinessRating, $priceRating, $contractorID, $jobID, $customerID);
+
+                if ($row) {
+                    $prefilledQualityRating = $row['qualityRating'];
+                    $prefilledCommunicationRating = $row['communicationRating'];
+                    $prefilledTimelinessRating = $row['timelinessRating'];
+                    $prefilledPriceRating = $row['priceRating'];
+                } else {
+                    echo '<script>alert("Error: Unable to save ratings.");</script>';
+                }
+            }
+        }
+    }
+}
+
+$db->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/ratings.css">
+</head>
+
+<body>
     <header>
         <div class="dropdown">
             <button class="dropbtn">...</button>
@@ -174,100 +155,126 @@
             </div>
         </div>
         <div class="welcome-user">
-            Welcome, <?php echo htmlspecialchars($customerID); ?><br>
-            Email: <?php echo htmlspecialchars($userEmail); ?>
+            Welcome, <?php echo $userFName; ?><br>
+            Email: <?php echo $userEmail; ?>
         </div>
     </header>
+
     <div class="centered-content">
         <div class="card">
             <h2>How would you rate this contractor?</h2>
             Quality of Work:
             <div class="stars" id="qualityStars">
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
+                <?php for ($i = 1; $i <= 5; $i++) : ?>
+                    <span class="fa fa-star" onclick="rateStar('qualityStars', <?php echo $i; ?>)"></span>
+                <?php endfor; ?>
             </div>
-            <br>
+            <br>        
             Communication:
             <div class="stars" id="communicationStars">
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
+                <?php for ($i = 1; $i <= 5; $i++) : ?>
+                    <span class="fa fa-star" onclick="rateStar('communicationStars', <?php echo $i; ?>)"></span>
+                <?php endfor; ?>
             </div>
             <br>
+
             Timeliness:
             <div class="stars" id="timelinessStars">
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
+                <?php for ($i = 1; $i <= 5; $i++) : ?>
+                    <span class="fa fa-star" onclick="rateStar('timelinessStars', <?php echo $i; ?>)"></span>
+                <?php endfor; ?>
             </div>
             <br>
+
             Price:
             <div class="stars" id="priceStars">
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
+                <?php for ($i = 1; $i <= 5; $i++) : ?>
+                    <span class="fa fa-star" onclick="rateStar('priceStars', <?php echo $i; ?>)"></span>
+                <?php endfor; ?>
             </div>
             <br>
+
             <form method="post" action="#">
                 <input type="hidden" name="qualityRating" id="qualityRating" value="0">
                 <input type="hidden" name="communicationRating" id="communicationRating" value="0">
                 <input type="hidden" name="timelinessRating" id="timelinessRating" value="0">
                 <input type="hidden" name="priceRating" id="priceRating" value="0">
+                <input type="hidden" name="jobID" value="<?php echo htmlspecialchars($_GET['jobID']); ?>">
                 <input type="button" value="Submit" onclick="submitRating()" class="submit-button">
             </form>
-            <div id="thankYouMessage"></div>
+
+             <div id="thankYouMessage"></div>
         </div>
     </div>
-    <script>
-        function submitRating() {
-            const qualityRating = document.getElementById('qualityStars').getElementsByClassName('fa-star checked').length;
-            const communicationRating = document.getElementById('communicationStars').getElementsByClassName('fa-star checked').length;
-            const timelinessRating = document.getElementById('timelinessStars').getElementsByClassName('fa-star checked').length;
-            const priceRating = document.getElementById('priceStars').getElementsByClassName('fa-star checked').length;
 
-            // Set the values of hidden input fields
-            document.getElementById('qualityRating').value = qualityRating;
-            document.getElementById('communicationRating').value = communicationRating;
-            document.getElementById('timelinessRating').value = timelinessRating;
-            document.getElementById('priceRating').value = priceRating;
+<script>
+  function rateStar(starSetId, rating) {
+            const starSet = document.getElementById(starSetId);
+            const stars = starSet.getElementsByClassName('fa-star');
+
+            for (let i = 0; i < stars.length; i++) {
+                const star = stars[i];
+                if (i < rating) {
+                    star.classList.add('checked');
+                } else {
+                    star.classList.remove('checked');
+                }
+            }
+
+            // Set the corresponding hidden input field value
+            const ratingInput = document.getElementById(starSetId.replace('Stars', 'Rating'));
+            if (ratingInput) {
+                ratingInput.value = rating;
+            }
+        }
+
+        function submitRating() {
+            // Validate ratings for all criteria
+            const qualityRating = document.getElementById('qualityRating') ? document.getElementById('qualityRating').value : 0;
+            const communicationRating = document.getElementById('communicationRating') ? document.getElementById('communicationRating').value : 0;
+            const timelinessRating = document.getElementById('timelinessRating') ? document.getElementById('timelinessRating').value : 0;
+            const priceRating = document.getElementById('priceRating') ? document.getElementById('priceRating').value : 0;
+
+            if (qualityRating === '0' || communicationRating === '0' || timelinessRating === '0' || priceRating === '0') {
+                alert('Please rate all criteria before submitting.');
+                return;
+            }
+
+            // Additional validation logic if needed
+
+            // Display thank-you message
+            displayThankYouMessage();
 
             // Submit the form
             document.forms[0].submit();
         }
 
         function displayThankYouMessage() {
-            const submitButton = document.querySelector('.submit-button');
             const thankYouMessage = document.getElementById('thankYouMessage');
-            thankYouMessage.textContent = 'Thank you for your rating';
-            thankYouMessage.style.color = '#3e8e41'; // Set color to green
+            if (thankYouMessage) {
+                thankYouMessage.innerHTML = 'Thank you for your rating';
+                thankYouMessage.style.color = '#3e8e41'; // Set color to green
+            }
         }
+		const jobID = <?php echo json_encode(htmlspecialchars($_GET['jobID'])); ?>;
+    const updatedRatings = <?php echo json_encode([
+        'qualityRating' => $prefilledQualityRating,
+        'communicationRating' => $prefilledCommunicationRating,
+        'timelinessRating' => $prefilledTimelinessRating,
+        'priceRating' => $prefilledPriceRating,
+    ]); ?>;
 
-        const stars = document.querySelectorAll('.stars');
+    // Prefill the star ratings based on the retrieved data
+    function prefillRatings() {
+        rateStar('qualityStars', updatedRatings.qualityRating);
+        rateStar('communicationStars', updatedRatings.communicationRating);
+        rateStar('timelinessStars', updatedRatings.timelinessRating);
+        rateStar('priceStars', updatedRatings.priceRating);
+    }
 
-        stars.forEach(starSet => {
-            Array.from(starSet.children).forEach(star => {
-                star.addEventListener('click', function () {
-                    const parent = this.parentElement;
-                    Array.from(parent.children).forEach((s, i) => {
-                        if (i <= Array.from(parent.children).indexOf(this)) {
-                            s.classList.add('checked');
-                        } else {
-                            s.classList.remove('checked');
-                        }
-                    });
-                });
-            });
-        });
-    </script>
+    // Call the prefillRatings function after the page loads
+    window.onload = prefillRatings;
+</script>
 </body>
 
 </html>
